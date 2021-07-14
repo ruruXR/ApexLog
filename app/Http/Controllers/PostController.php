b67b5890ab938aa3b4c32d21d48a607eda10e675
ruruXR <dirty.soil@icloud.com>
2021-07-14 22:13:51 +0900

    8-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
-    }
+        return view('index')->with(['posts' => $post->getPaginateByLimit(1)]);
+    } 
 }
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::get('/posts','PostController@index');
+Route::get('/', 'PostController@index');
