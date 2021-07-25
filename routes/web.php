b5cd8d78622ea89c9c7c321704410a5de541fbfe
ruruXR <dirty.soil@icloud.com>
2021-07-26 07:39:32 +0900

    8-6

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ Route::get('/posts/{post}','PostController@show');
 Route::get('/posts/{post}/edit', 'PostController@edit');
 Route::put('/posts/{post}', 'PostController@update');
 Route::post('/posts', 'PostController@store');
+Route::delete('/posts/{post}', 'PostController@destroy');
