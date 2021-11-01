9948da2319492e743cbf40d7ed19ea63314592d8
ruruXR <dirty.soil@icloud.com>
2021-11-01 14:12:08 +0900

    テスト

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,17 +6,32 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Post;
 use App\User;
+use App\Category;
 
 class ProfileController extends Controller
 {
     public function profile (User $user) 
     {
+        $category = new Category;
+        $categories = $category->getList();
+        
+        // 検索に使う情報の取得
+        $category_id = $request->category_id;
+        $searchword = $request->searchword;
+        $user_id = $request->user_id;
+        
         $posts = Post::with(['comments', 'category'])
+        ->categoryAt($category_id)
+        ->fuzzyNameMessage($searchword)
         ->PostAt($user->id)
         ->Paginate(10);
-        return view('profile')->with([
+        
+        return view('index')->with([
             'posts' => $posts,
+            'categories' => $categories,
+            'category_id' => $category_id,
+            'searchword' => $searchword,
             'user' => $user,
-            ]); 
+            ]);
     }
 }
